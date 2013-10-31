85720cffec13478433a1a6c0e0ab23349db0075c
Angie Gove <ang.gove@gmail.com>
2013-10-31 17:55:51 +1100

    update authentication method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,16 @@ class ApplicationController < ActionController::Base
   private #must go at bottom of file it effects everything after it.
   #if you make this private wont effectif we name something else authenitication
     def authentication
-      @user_authenticated = User.find session[:user_id] unless session[:user_id].nil?
-      @company_authenticated = Company.find session[:company_id] unless session[:company_id].nil?
+
+      return unless (session[:user_id] || session[:company_id]) # Check for IDs to authenticate.
+
+      @user_authenticated = User.find_by_id session[:user_id] unless session[:user_id].nil?
+      @company_authenticated = Company.find_by_id session[:company_id] unless session[:company_id].nil?
+
+      # If we didn't log anyone in, clear out the sessions so we don't try again.
+      if @user_authenticated.nil? && @company_authenticated.nil?
+        session[:user_id] = nil
+        session[:company_id] = nil
+      end
     end
 end
