2c0c162c4fec80284a9bc5a6def3918ca4297dec
Angie Gove <ang.gove@gmail.com>
2013-11-01 13:15:44 +1100

    added S3 settings

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,19 +1,13 @@
 CarrierWave.configure do |config|
+  config.fog_credentials = {
+    :provider       => 'AWS'
+    :aws_access_key_id => ENV['S3_KEY'],
+    :aws_secret_access_key => ENV['S3_SECRET'],
+    :region => 'us-west-1'
 
-  #if Rails.env.development? || Rails.env.test?
-  #  config.storage = :file
-  #else 
-    config.fog_credentials = {
-      :provider              => 'AWS',                        # required
-      :aws_access_key_id      => ENV['S3_KEY'],                        # required
-      :aws_secret_access_key  => ENV['S3_SECRET'],                        # required
-      :region                => 'us-west-1'                  # optional, defaults to 'us-east-1'
-      #:host                  => 's3.example.com',            # optional, defaults to nil
-      #:endpoint              => 'https://s3.example.com:8080' # optional, defaults to nil
-    }
-    config.fog_directory  = ENV['S3_BUCKET']                    # required
-    config.fog_public    = true                                  # optional, defaults to true
-    config.fog_attributes = {} # {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
-  #end
+  }
 
+  config.fog_directory = ENV['S3_BUCKET']
+  config.fog_public  = true
+  config.fog_attributes = {}
 end
