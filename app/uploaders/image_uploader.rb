5343cde1223ea43bb2ce6e004c60e91d5078b440
Angie Gove <ang.gove@gmail.com>
2013-11-11 15:04:44 +1100

    fixing password digest and form

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -7,8 +7,11 @@ class ImageUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  # storage :file
-  storage :fog
+  if Rails.env.development?
+    storage :file
+  else
+    storage :fog
+  end
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
@@ -20,7 +23,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   def default_url
     # For Rails 3.1+ asset pipeline compatibility:
     # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
-  
+
     "/assets/placeholder.jpg"
   end
 
